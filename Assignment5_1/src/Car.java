ec004adde500eac1a7cddb4a24ba072f022fdfe9
Sieun Kim <80445374+seplease@users.noreply.github.com>
2023-07-06 22:37:06 +0900

    Add files via upload

---
+++ Assignment5_1/src/Car.java
@@ -0,0 +1,10 @@
+
+public class Car {
+	String color;
+	int speed;
+	int gear;
+	
+	void print() {
+		System.out.println("color : " + color + ", speed : "+ speed);
+	}
+}
---
+++ Assignment5_1/src/CarTest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car obj;
+		obj = new Car();		
+		
+		obj.color = "red";
+		obj.speed = 150;
+		
+		obj.print();
+	}
+}
