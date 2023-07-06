0f3a31d1d7a4d119e2c5a484455633b122ee9a59
Sieun Kim <80445374+seplease@users.noreply.github.com>
2023-07-06 22:46:03 +0900

    Add files via upload

---
+++ Assignment10_3/src/Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+	private String name;
+	private String type;
+		
+	public String getName() {	return name;	};
+	public void setName(String name) {	this.name = name;	};
+	public String getType() {	return type;	};
+	public void setType(String type) { this.type = type;	};		
+		
+	public Dog(String name, String type) {
+		this.name = name;
+		this.type = type;
+	}
+	
+	Dog obj = new Dog(name, type);
+	public String toString() {
+		return obj.getName() + ", " + obj.getType();
+	}
+}
