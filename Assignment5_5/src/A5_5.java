90ad38769798648f047afb5738306a6fc2f69a6b
Sieun Kim <80445374+seplease@users.noreply.github.com>
2023-07-06 22:38:17 +0900

    Add files via upload

---
+++ Assignment5_5/src/A5_5.java
@@ -0,0 +1,17 @@
+
+public class A5_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student obj;
+		obj = new Student();		
+		
+		obj.name = "Kim";
+		obj.rollno = 20180001;
+		obj.age = 20;
+		
+		System.out.println("학생의 이름: " + obj.name);
+		System.out.println("학생의 학번: " + obj.rollno);
+		System.out.println("학생의 나이: " + obj.age);
+	}
+}
