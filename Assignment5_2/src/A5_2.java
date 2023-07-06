f98800041fe481c49789cfef1b461d8ea2f0eb85
Sieun Kim <80445374+seplease@users.noreply.github.com>
2023-07-06 22:37:22 +0900

    Add files via upload

---
+++ Assignment5_2/src/A5_2.java
@@ -0,0 +1,20 @@
+
+public class A5_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		show();
+		System.out.println("메소드 호출하기 전");
+		show();
+		System.out.println("메소드 호출한 후");
+		show();
+	}
+		
+	public static void show() {
+		int i;
+		for (i = 0; i < 10; i++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
